9170c41e857b3bb5a73c99c5664e9f83ad0fd044
Jhon Fredy <jfinfocesde@gmail.com>
2024-05-02 22:13:16 -0500

    2-5-2024

--- src/app/course/page.menu.ts
+++ src/app/course/page.menu.ts
@@ -196,4 +196,21 @@ export const itemsInfo: itemsInfoType[] = [
             // },
         ]
     },    
+    {
+        href: "",
+        label: "Semana 11",
+        date: '2024-5-3',
+        submenu: [
+            {
+                href: "/course/contents/semana11",
+                label: "Contenido",
+                date: '2024-5-3',
+            },
+            // {
+            //     href: "/course/activities/actividad6",
+            //     label: "Actividad 6",
+            //     date: '2024-4-19',
+            // },
+        ]
+    },    
 ]
